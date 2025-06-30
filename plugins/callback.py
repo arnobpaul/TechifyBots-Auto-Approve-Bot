0c07bb998fe8eb0adc2e0340a74641ce7d91d957
arnobpaul <117028354+arnobpaul@users.noreply.github.com>
2025-06-30 13:48:11 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -4,17 +4,16 @@ from Script import text
 from config import ADMIN
 
 @Client.on_callback_query()
-async def callback_query_handler(bot: Client, message: Message, query: CallbackQuery):
+async def callback_query_handler(client, query: CallbackQuery):
     if query.data == "start":
-        tb = await bot.get_me()
         await query.message.edit_text(
             text.START.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆', url=f"https://telegram.me/{tb.username}?startgroup=join_otherbots&admin=post_messages+edit_messages+delete_messages+restrict_members+invite_users+pin_messages+promote_members+manage_video_chats+manage_chat")],
+                [InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆', url=f"https://telegram.me/Anjakakallaleech_bot?startgroup=join_otherbots&admin=post_messages+edit_messages+delete_messages+restrict_members+invite_users+pin_messages+promote_members+manage_video_chats+manage_chat")],
                 [InlineKeyboardButton('ℹ️ ᴀʙᴏᴜᴛ', callback_data='about'),
                  InlineKeyboardButton('🛠️ ʜᴇʟᴘ', callback_data='help')],
-                [InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ⇆', url=f"https://telegram.me/{tb.username}?startchannel=join_otherbots&admin=post_messages+edit_messages+delete_messages+restrict_members+invite_users+pin_messages+promote_members+manage_video_chats+manage_chat")]
+                [InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ⇆', url=f"https://telegram.me/Anjakakallaleech_bot?startchannel=join_otherbots&admin=post_messages+edit_messages+delete_messages+restrict_members+invite_users+pin_messages+promote_members+manage_video_chats+manage_chat")]
             ])
         )
 
